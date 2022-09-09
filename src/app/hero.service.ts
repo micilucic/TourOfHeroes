b484cad303a16900f9fd70d17ab347acb1e6dbd3
Micilucic <lucicmili@gmail.com>
2022-09-09 14:11:20 +0200

    Add navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,20 +3,30 @@ import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService: MessageService) { }
-
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes.')
-    return heroes;
+  constructor(private messageService: MessageService, private http: HttpClient) {
   }
 
+  getHeroes(): Observable<Hero[]> {
+    //const heroes = of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: Number) {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ${message}');
+  }
 }
